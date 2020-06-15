24d3cf8d752029743d30a6daddf770f8e18759fa
Tim Kormachev <tkormachev@alfabank.ru>
2020-06-15 22:21:56 +0300

    HW02 1 InMemoryUserRepository added

---
+++ src/main/java/ru/javawebinar/topjava/util/UserUtil.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserUtil {
+    public static final List<User> USERS = Arrays.asList(
+            new User(null, "B", "test_3@email.com", "qwerty123", Role.USER),
+            new User(null, "A", "test_2@email.com", "qwerty123", Role.USER),
+            new User(null, "C", "test_1@email.com", "qwerty123", Role.USER)
+    );
+
+}
