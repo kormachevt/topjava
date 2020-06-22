10dc2ca8202ebd05f7ac02ee6614085b0dd616a6
Tim Kormachev <tkormachev@alfabank.ru>
2020-06-22 09:13:33 +0300

    HW03 2  Meals table added

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -22,4 +23,18 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+create table meals
+(
+    id integer default nextval('global_seq'::regclass) not null
+        constraint meals_pk
+            primary key,
+    date_time timestamp default now() not null,
+    description varchar not null,
+    calories integer not null,
+    user_id integer not null
+        constraint meals_user_id_fk
+            references users
+            on delete cascade
 );
